8c333889608c9b1f1f0a5438c9b9b96b72562a8b
Alex Chaphiv <achaphiv@gmail.com>
2014-01-28 05:38:40 -0500

    Add script for building for openjdk-fontfix

---
+++ openjdk-fontfix.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -ex
+
+PACKAGE='openjdk-7'
+PATCH_NAME='fontfix.patch'
+
+function build {
+	DIST=$1
+	VERSION=$2
+
+	STARTING_DIR=$(pwd)
+
+	BUILD_DIR=build-${PACKAGE}-${DIST}
+	rm -r ${BUILD_DIR} || true
+	mkdir ${BUILD_DIR}
+	cd ${BUILD_DIR}
+
+	apt-get source ${PACKAGE}=${VERSION}
+
+	cd ${PACKAGE}*/
+
+	cp ${STARTING_DIR}/${PATCH_NAME} debian/patches/
+
+	patch -p1 < ${STARTING_DIR}/add_fontfix_patch.patch
+
+	CHANGELOG=$(mktemp)
+
+	cat <<-EOF > ${CHANGELOG}
+	openjdk-7 (${VERSION}ppa1) ${DIST}; urgency=low
+
+	  * Add fontfix patch
+	  * Use fontfix patch
+
+	 -- Bob Chez <no1wantdthisname@gmail.com>  $(date -R)
+
+	EOF
+
+	cat debian/changelog >> ${CHANGELOG}
+
+	mv ${CHANGELOG} debian/changelog
+
+	debuild -S -sd
+
+	cd ../
+
+	dput openjdk-fontfix *changes
+}
+
+build precise 7u51-2.4.4-0ubuntu0.12.04.2
+build saucy 7u51-2.4.4-0ubuntu0.13.10.1
+
