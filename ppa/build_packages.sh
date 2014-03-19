c80dabb223200ebeb490be3f26f9e0cc4797dcc3
Alex Chaphiv <achaphiv@palantir.com>
2014-03-18 20:39:27 -0400

    Update build script for new saucy freetype

--- ppa/build_packages.sh
+++ ppa/build_packages.sh
@@ -1,9 +1,71 @@
 #!/bin/sh
 
-VERSION='2.5.2-1ubuntu1'
+set -ex
 
-mkdir build
-cd build
+PACKAGE='freetype'
+PATCH_NAME='freetype-entire-infinality-patchset-20130514-01.patch'
+PPA_VERSION='ppa1infinality20130515'
 
-apt-get source freetype=${VERSION}
+STARTING_DIR=$(pwd)
+
+function build {
+	DIST=$1
+	VERSION=$2
+
+	BUILD_DIR=build-${PACKAGE}-${DIST}
+	rm -r ${BUILD_DIR} || true
+	mkdir ${BUILD_DIR}
+	cd ${BUILD_DIR}
+
+	apt-get source ${PACKAGE}=${VERSION}
+
+	cd ${PACKAGE}*/
+
+  # ignore compiler warnings
+  sed -i -e '/export DEB_CFLAGS_MAINT_APPEND := -Werror/d' debian/rules
+  
+	# remove since infinality already enables this
+	sed -i -e '/enable-subpixel-rendering.patch/d' debian/patches-freetype/series
+  
+  # add/use infinality patch
+  cp ${STARTING_DIR}/${PATCH_NAME} debian/patches-freetype/
+	echo ${PATCH_NAME} >> debian/patches-freetype/series
+
+  # Level | Failure
+  # 0     | impossible (all checks disabled)
+  # 1     | symbols have disappeared
+  # 2     | symbols have been introduced
+  # 3     | libraries have disappeared
+  # 4     | libraries have been introduced
+  sed -i -e 's/DPKG_GENSYMBOLS_CHECK_LEVEL = 4/DPKG_GENSYMBOLS_CHECK_LEVEL = 1/' debian/rules
+  
+	CHANGELOG=$(mktemp)
+
+	cat <<-EOF > ${CHANGELOG}
+	${PACKAGE} (${VERSION}${PPA_VERSION}) ${DIST}; urgency=low
+
+	  * Ignore compiler warnings
+	  * Add infinality patch
+	  * Disable subpixel patch
+	  * Use infinality patch
+	  * Ignore extra symbols
+
+	 -- Bob Chez <no1wantdthisname@gmail.com>  $(date -R)
+
+	EOF
+
+	cat debian/changelog >> ${CHANGELOG}
+
+	mv ${CHANGELOG} debian/changelog
+
+	debuild -S -sd
+
+	cd ../
+
+	dput -c ${STARTING_DIR}/dput.cf ppa *changes
+
+	cd ${STARTING_DIR}
+}
+
+build saucy 2.4.12-0ubuntu1.1
 
