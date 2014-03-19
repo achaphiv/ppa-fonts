d6981a50acc544b654ed3c194b1cf412f8206f7d
Alex Chaphiv <achaphiv@palantir.com>
2014-03-18 20:45:51 -0400

    Fix whitespace formatting

--- ppa/build_packages.sh
+++ ppa/build_packages.sh
@@ -21,24 +21,24 @@ function build {
 
 	cd ${PACKAGE}*/
 
-  # ignore compiler warnings
-  sed -i -e '/export DEB_CFLAGS_MAINT_APPEND := -Werror/d' debian/rules
-  
+	# ignore compiler warnings
+	sed -i -e '/export DEB_CFLAGS_MAINT_APPEND := -Werror/d' debian/rules
+
 	# remove since infinality already enables this
 	sed -i -e '/enable-subpixel-rendering.patch/d' debian/patches-freetype/series
-  
-  # add/use infinality patch
-  cp ${STARTING_DIR}/${PATCH_NAME} debian/patches-freetype/
+
+	# add/use infinality patch
+	cp ${STARTING_DIR}/${PATCH_NAME} debian/patches-freetype/
 	echo ${PATCH_NAME} >> debian/patches-freetype/series
 
-  # Level | Failure
-  # 0     | impossible (all checks disabled)
-  # 1     | symbols have disappeared
-  # 2     | symbols have been introduced
-  # 3     | libraries have disappeared
-  # 4     | libraries have been introduced
-  sed -i -e 's/DPKG_GENSYMBOLS_CHECK_LEVEL = 4/DPKG_GENSYMBOLS_CHECK_LEVEL = 1/' debian/rules
-  
+	# Level | Failure
+	# 0     | impossible (all checks disabled)
+	# 1     | symbols have disappeared
+	# 2     | symbols have been introduced
+	# 3     | libraries have disappeared
+	# 4     | libraries have been introduced
+	sed -i -e 's/DPKG_GENSYMBOLS_CHECK_LEVEL = 4/DPKG_GENSYMBOLS_CHECK_LEVEL = 1/' debian/rules
+
 	CHANGELOG=$(mktemp)
 
 	cat <<-EOF > ${CHANGELOG}
