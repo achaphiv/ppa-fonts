92eff774574d0d35c245b758ae16146591475260
achaphiv <achaphiv@gmail.com>
2015-04-22 17:21:12 -0400

    Update versions for openjdk-7

--- openjdk-fontfix/build_packages.sh
+++ openjdk-fontfix/build_packages.sh
@@ -48,7 +48,7 @@ function build {
 	cd ${STARTING_DIR}
 }
 
-#build openjdk-7 precise 7u75-2.5.4-1~precise1 ppa3 enable_infinality.patch
-#build openjdk-7 trusty 7u75-2.5.4-1~trusty1 ppa2 enable_infinality.patch
-#build openjdk-7 utopic 7u75-2.5.4-1~utopic1 ppa2 enable_infinality.patch
-build openjdk-8 utopic 8u40~b09-1 ppa2 enable_tuxjdk.patch
+build openjdk-7 precise 7u79-2.5.5-0ubuntu0.12.04.1 ppa1 enable_infinality.patch
+build openjdk-7 trusty 7u79-2.5.5-0ubuntu0.14.04.2 ppa1 enable_infinality.patch
+build openjdk-7 utopic 7u79-2.5.5-0ubuntu0.14.10.2 ppa1 enable_infinality.patch
+#build openjdk-8 utopic 8u40~b09-1 ppa2 enable_tuxjdk.patch
