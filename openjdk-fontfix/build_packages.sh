5a85b3293cab74ffe798178ddd0738d969ae97a2
Alex Chaphiv <achaphiv@gmail.com>
2015-11-30 11:00:26 +0000

    Update for jdk 7u91

--- openjdk-fontfix/build_packages.sh
+++ openjdk-fontfix/build_packages.sh
@@ -49,11 +49,11 @@ function build {
 }
 
 #build openjdk-7 precise 7u85-2.6.1-5ubuntu0.12.04.1 ppa1 enable_infinality.patch
-#build openjdk-7 trusty 7u85-2.6.1-5ubuntu0.14.04.1 ppa1 enable_infinality.patch
+build openjdk-7 trusty 7u91-2.6.3-0ubuntu0.14.04.1 ppa1 enable_infinality.patch
 #build openjdk-8 trusty 8u45-b14-1~14.04 ppa1 enable_tuxjdk.patch
 #build openjdk-7 utopic 7u79-2.5.5-0ubuntu0.14.10.2 ppa1 enable_infinality.patch
 #build openjdk-8 utopic 8u40~b09-1 ppa2 enable_tuxjdk.patch
-#build openjdk-7 vivid 7u85-2.6.1-5ubuntu0.15.04.1 ppa1 enable_infinality.patch
+build openjdk-7 vivid 7u91-2.6.3-0ubuntu0.15.04.1 ppa1 enable_infinality.patch
 #build openjdk-8 vivid 8u45-b14-1 ppa1 enable_tuxjdk.patch
-#build openjdk-7 wily 7u85-2.6.1-5ubuntu0.15.10.1 ppa1 enable_infinality.patch
-build openjdk-8 wily 8u66-b17-1 ppa1 enable_tuxjdk.patch
+build openjdk-7 wily 7u91-2.6.3-0ubuntu0.15.10.1 ppa1 enable_infinality.patch
+#build openjdk-8 wily 8u66-b17-1 ppa1 enable_tuxjdk.patch
