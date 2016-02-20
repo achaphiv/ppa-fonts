a880b5099075c4d84305e72c3bd2eb642bb16741
Alex Chaphiv <achaphiv@gmail.com>
2016-02-20 17:53:42 +0000

    Update opendjk-8 for trusty

--- openjdk-fontfix/build_packages.sh
+++ openjdk-fontfix/build_packages.sh
@@ -50,7 +50,7 @@ function build {
 
 #build openjdk-7 precise 7u91-2.6.3-0ubuntu0.12.04.1 ppa1 enable_infinality.patch
 #build openjdk-7 trusty 7u91-2.6.3-0ubuntu0.14.04.1 ppa1 enable_infinality.patch
-build openjdk-8 trusty 8u72-b05-5~trusty1 ppa1 enable_tuxjdk.patch
+build openjdk-8 trusty 8u72-b15-1~trusty1 ppa1 enable_tuxjdk.patch
 #build openjdk-7 utopic 7u79-2.5.5-0ubuntu0.14.10.2 ppa1 enable_infinality.patch
 #build openjdk-8 utopic 8u40~b09-1 ppa2 enable_tuxjdk.patch
 #build openjdk-7 vivid 7u91-2.6.3-0ubuntu0.15.04.1 ppa1 enable_infinality.patch
