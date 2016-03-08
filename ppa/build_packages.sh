a4c56a9f8e6a948f5c60cef635d3a79084b3c46b
Alex Chaphiv <achaphiv@gmail.com>
2016-03-08 02:48:45 -0500

    Build freetype for xenial

--- ppa/build_packages.sh
+++ ppa/build_packages.sh
@@ -70,4 +70,5 @@ function build {
 #build trusty 2.5.2-1ubuntu2.5 'infinality-2.5.3.patch' 'ppa1bohoomileb5a6af0e99ec0d1c25521b6f8196106508c9360'
 #build utopic 2.5.2-2ubuntu1.1 'infinality-2.5.3.patch' 'ppa1bohoomileb5a6af0e99ec0d1c25521b6f8196106508c9360'
 #build vivid 2.5.2-2ubuntu3.1 'infinality-2.5.3.patch' 'ppa1bohoomileb5a6af0e99ec0d1c25521b6f8196106508c9360'
-build wily 2.5.2-4ubuntu2 'infinality-2.5.3.patch' 'ppa1bohoomileb5a6af0e99ec0d1c25521b6f8196106508c9360'
+#build wily 2.5.2-4ubuntu2 'infinality-2.5.3.patch' 'ppa1bohoomileb5a6af0e99ec0d1c25521b6f8196106508c9360'
+build xenial 2.6.1-0.1ubuntu1 '03-infinality-2.6.2-2015.12.05.patch' 'ppa1bohoomil20151205'
