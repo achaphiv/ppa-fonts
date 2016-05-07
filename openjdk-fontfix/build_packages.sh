c83c1dfa8e3c0a16b418c010375e51059d3dce8d
Alex Chaphiv <achaphiv@gmail.com>
2016-05-06 22:16:02 -0400

    Build openjdk 7/8 security updates

--- openjdk-fontfix/build_packages.sh
+++ openjdk-fontfix/build_packages.sh
@@ -48,18 +48,18 @@ function build {
 	cd ${STARTING_DIR}
 }
 
-latest_7=7u95-2.6.4-0ubuntu0
+latest_7=7u101-2.6.6-0ubuntu0.12.04.1
 
-#build openjdk-7 precise $latest_7.12.04.2 ppa1 enable_infinality.patch
+build openjdk-7 precise 7u101-2.6.6-0ubuntu0.12.04.1 ppa1 enable_infinality.patch
 #build openjdk-8 precise 8u72-b15-1~precise1 ppa1 enable_tuxjdk.patch
-#build openjdk-7 trusty $latest_7.14.04.2 ppa1 enable_infinality.patch
+build openjdk-7 trusty openjdk-7 7u101-2.6.6-0ubuntu0.14.04.1 enable_infinality.patch
 #build openjdk-7 utopic 7u79-2.5.5-0ubuntu0.14.10.2 ppa1 enable_infinality.patch
 #build openjdk-8 utopic 8u40~b09-1 ppa2 enable_tuxjdk.patch
 #build openjdk-7 vivid $latest_7.15.04.1 ppa1 enable_infinality.patch
 #build openjdk-8 vivid 8u45-b14-1 ppa1 enable_tuxjdk.patch
-#build openjdk-7 wily $latest_7.15.10.2 ppa1 enable_infinality.patch
-#build openjdk-8 wily 8u66-b17-1 ppa1 enable_tuxjdk.patch
+build openjdk-7 wily 7u101-2.6.6-0ubuntu0.15.10.1 ppa1 enable_infinality.patch
+build openjdk-8 wily 8u91-b14-0ubuntu4~15.10.1 enable_tuxjdk.patch
 
 #build openjdk-7 xenial 7u95-2.6.4-1 ppa1 enable_infinality.patch
-build openjdk-8 xenial 8u77-b03-3ubuntu1 ppa1 enable_tuxjdk.patch
+build openjdk-8 xenial 8u91-b14-0ubuntu4~16.04.1 ppa1 enable_tuxjdk.patch
 
