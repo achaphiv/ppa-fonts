9abfd642056e4fe2c3ab5b7977019a749bd229da
Alex Chaphiv <achaphiv@gmail.com>
2016-09-06 21:56:34 -0400

    Remove redundant patch

--- ppa/build_packages.sh
+++ ppa/build_packages.sh
@@ -39,6 +39,9 @@ function build_freetype {
 	cp ${STARTING_DIR}/${PACKAGE}/${PATCH_NAME} debian/patches-freetype/
 	echo ${PATCH_NAME} >> debian/patches-freetype/series
 
+	# Redundant
+	sed -i -e '/enable-subpixel-rendering.patch/d' debian/patches-freetype/series
+
 	# remove since 2.6.5 changes things
 	sed -i -e '/0001-Revert-pcf-Signedness-fixes.patch/d' debian/patches-freetype/series
 	sed -i -e '/compiler_hardening_fixes.patch/d' debian/patches-ft2demos/series
@@ -81,5 +84,5 @@ function build_freetype {
 #build_freetype utopic 2.5.2-2ubuntu1.1 'infinality-2.5.3.patch' 'ppa1bohoomileb5a6af0e99ec0d1c25521b6f8196106508c9360'
 #build_freetype vivid 2.5.2-2ubuntu3.1 'infinality-2.5.3.patch' 'ppa1bohoomileb5a6af0e99ec0d1c25521b6f8196106508c9360'
 #build_freetype wily 2.5.2-4ubuntu2 'infinality-2.5.3.patch' 'ppa1bohoomileb5a6af0e99ec0d1c25521b6f8196106508c9360'
-build_freetype xenial 2.6.1-0.1ubuntu2 'enable-subpixel-hinting.patch' 'ppa5'
+build_freetype xenial 2.6.1-0.1ubuntu2 'enable-subpixel-hinting.patch' 'ppa6'
 #build_fontconfig xenial 2.11.94-0ubuntu1 ppa1
