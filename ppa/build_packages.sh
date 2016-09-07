ed153ad4cea0e71762872510a38d58c14fcd0f30
Alex Chaphiv <achaphiv@gmail.com>
2016-09-07 02:27:11 -0400

    Revert "Switch to freetype's infinality mode"
    Seems like freetype's new mode is okay.
    
    This reverts commit 0c8d4b039bcfb2c5c893d0fd35d0b59c42e05d80.

--- ppa/build_packages.sh
+++ ppa/build_packages.sh
@@ -84,5 +84,5 @@ function build_freetype {
 #build_freetype utopic 2.5.2-2ubuntu1.1 'infinality-2.5.3.patch' 'ppa1bohoomileb5a6af0e99ec0d1c25521b6f8196106508c9360'
 #build_freetype vivid 2.5.2-2ubuntu3.1 'infinality-2.5.3.patch' 'ppa1bohoomileb5a6af0e99ec0d1c25521b6f8196106508c9360'
 #build_freetype wily 2.5.2-4ubuntu2 'infinality-2.5.3.patch' 'ppa1bohoomileb5a6af0e99ec0d1c25521b6f8196106508c9360'
-build_freetype xenial 2.6.1-0.1ubuntu2 'enable-subpixel-hinting.patch' 'ppa7'
+build_freetype xenial 2.6.1-0.1ubuntu2 'enable-subpixel-hinting.patch' 'ppa6'
 #build_fontconfig xenial 2.11.94-0ubuntu1 ppa1
